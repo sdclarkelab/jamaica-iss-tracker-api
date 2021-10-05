198819542a7db5d7a51d8af9ec8f79f6a0c42836
Stephen Clarke <sdclarke876@gmail.com>
2021-10-05 00:06:59 -0500

    feat: search for sighting location using url query

--- scrapers/spot_the_station_scraper.py
+++ scrapers/spot_the_station_scraper.py
@@ -1,26 +1,38 @@
 import utils
 
 
-def get_spot_the_station_data(location):
+def get_spot_the_station_data(url):
     """
     get page based on the location provided.
-    :param location:
+    :param url:
     :return:
     """
-    url = f'https://spotthestation.nasa.gov/sightings/view.cfm?country=Jamaica&region=None&city={location}'
+    sighting_info = {}
     soup = utils.get_soup(url, 3)
-    return _extract_sighting_dates(soup)
+
+    sighting_info['sighting_period'] = _extract_sighting_period(soup)
+    sighting_info['sighting_dates'] = _extract_sighting_dates(soup)
+
+    return sighting_info
+
+
+def _extract_sighting_period(soup):
+    sighting_info = soup.find('div', class_="table-responsive")
+    sighting_period_p_tag = sighting_info.find('p')
+
+    return sighting_period_p_tag.get_text()
 
 
 def _extract_sighting_dates(soup):
     sighting_dates = []
     sightings_table = soup.find('table')
 
-    for row in sightings_table.find_all('tr'):
-        cells = row.find_all('td')
+    if sightings_table:
+        for row in sightings_table.find_all('tr'):
+            cells = row.find_all('td')
 
-        if len(cells) == 6:
-            date = cells[0].find(text=True)
-            sighting_dates.append(date)
+            if len(cells) == 6:
+                date = cells[0].find(text=True)
+                sighting_dates.append(date)
 
     return sighting_dates
