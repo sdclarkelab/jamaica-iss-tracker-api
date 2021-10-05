9fd51ab1d39616ce57f9f97491447f776e428628
Stephen Clarke <44792415+sdclarkelab@users.noreply.github.com>
2021-10-04 23:00:25 -0500

    feat: search for sighting location using url query

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import os
+import uvicorn
+from urllib.parse import urljoin, urlencode
 from fastapi import FastAPI
 
 import scrapers.spot_the_station_scraper as spot_the_station_scraper
@@ -5,9 +8,34 @@ import scrapers.spot_the_station_scraper as spot_the_station_scraper
 app = FastAPI()
 
 
-@app.get("/")
-async def root():
-    kingston_sighting_dates = spot_the_station_scraper.get_spot_the_station_data('Kingston')
-    MoBay_sighting_dates = spot_the_station_scraper.get_spot_the_station_data('Montego_Bay')
-    return {"Kingston": {"sighting_dates": kingston_sighting_dates},
-            "Montego_Bay": {"sighting_dates": MoBay_sighting_dates}}
+@app.get("/api/v1/iss/sightings/")
+async def sightings(country: str = 'Jamaica', region: str = 'None', city_names: str = 'Kingston,Montego_Bay'):
+
+    city_sightings = list()
+    city_urls = list()
+
+    if city_names:
+        city_names = city_names.strip().split(',')
+
+        for city_name in city_names:
+            query = "?" + urlencode(dict(country=country, region=region, city=city_name))
+            base_url = os.getenv("SPOT_THE_STATION_URL")
+            spot_the_station_url = urljoin(base_url, query)
+
+            city_urls.append(dict(city=city_name, url=spot_the_station_url))
+
+        for city_url in city_urls:
+            sighting_detail = spot_the_station_scraper.get_spot_the_station_data(city_url['url'])
+
+            city_sighting = dict(city_name=city_url['city'], sighting_detail=sighting_detail)
+            city_sightings.append(city_sighting)
+
+    return {
+        "country_name": country,
+        "cities": city_sightings,
+        "reference_link": "https://spotthestation.nasa.gov/"
+    }
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="localhost", port=8000)
+
